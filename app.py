1da7be1b617c6a78732812da2b5885b7ed52ed76
Felipe <lipe.h.r@gmail.com>
2024-01-18 16:11:09 -0300

    :sparkles: feat: login, logout, list, retrieve and update

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "s4135w+r-0^+@**u8d1g*-3s%y)bk(b^a83v%m7udf1+f1abp^"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+CORS(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
 
 class Product(db.Model):
@@ -14,11 +28,38 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data.get("username")).first()
+
+    if user and data.get("password") == user.password:
+        login_user(user)
+        return jsonify({"message": "Logged in successfully"})
+
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successfully"})
+
+
 @app.route("/api/products/add", methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if "name" in data and "price" in data:
-        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        product = Product(
+            name=data["name"], price=data["price"], description=data.get("description", "")
+        )
         db.session.add(product)
         db.session.commit()
         return jsonify({"message": "Product added successfully"})
@@ -27,6 +68,7 @@ def add_product():
 
 
 @app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -35,6 +77,59 @@ def delete_product(product_id):
         return jsonify({"message": "Product deleted successfully"})
 
     return jsonify({"message": "Product not found"}), 404
+
+
+@app.route("/api/products/<int:product_id>", methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify(
+            {
+                "id": product.id,
+                "name": product.name,
+                "price": product.price,
+                "description": product.description,
+            }
+        )
+
+    return jsonify({"message": "Product not found"}), 404
+
+
+@app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+@login_required
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+
+    data = request.json
+    if "name" in data:
+        product.name = data["name"]
+
+    if "price" in data:
+        product.price = data["price"]
+
+    if "description" in data:
+        product.description = data["description"]
+
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"})
+
+
+@app.route("/api/products", methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    products_list = []
+
+    for product in products:
+        products_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        products_list.append(products_data)
+
+    return jsonify(products_list)
 
 
 @app.route("/")
